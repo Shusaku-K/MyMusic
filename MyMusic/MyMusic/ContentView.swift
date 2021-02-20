437c15f8260015e321590561d0fe7fa0d61034bd
Shusaku-K <77326356+Shusaku-K@users.noreply.github.com>
2021-02-21 03:53:03 +0900

    Update ContentView.swift

--- MyMusic/MyMusic/ContentView.swift
+++ MyMusic/MyMusic/ContentView.swift
@@ -9,27 +9,35 @@ import SwiftUI
 
 struct ContentView: View {
     
+    // 音を鳴らすためのSoundPlayerクラスを生成
     let soundPlayer = SoundPlayer()
     
     
     var body: some View {
+        //ZStack = 奥行き。背景画像を設置。
         ZStack {
             Image("background")
                 .resizable()
                 .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                 .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                             
+            //水平方向にレイアウト。ここではボタンを2つ横に設置。
             HStack {
+                // シンバルボタン
                 Button(action: {
-                    
+                    // アクション:シンバルの音を鳴らす
                     soundPlayer.cymbalPlay()
                     
                 }) {
+                    // 画像を表示する
                     ButtonImageView(imageName: "cymbal")
                 }
                 
                 Button(action: {
+                    // アクション:ギターの音を鳴らす
                     soundPlayer.guitarPlay()
                 }) {
+                    // 画像を表示する
                     ButtonImageView(imageName: "guitar")
                 }
             }
