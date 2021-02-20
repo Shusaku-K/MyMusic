10d3a61363feaae732314e1fb86454b7b9144a67
Shusaku-K <77326356+Shusaku-K@users.noreply.github.com>
2021-02-21 04:39:12 +0900

    Update ButtonImageView.swift

--- MyMusic/MyMusic/ButtonImageView.swift
+++ MyMusic/MyMusic/ButtonImageView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct ButtonImageView: View {
-    
+    // 画像ファイル名
     let imageName: String
     
     var body: some View {
+        // 画像を表示する
         Image(imageName)
+        // ボタン内の画像をカラー画像となるように指定する
             .renderingMode(.original)
     }
 }
