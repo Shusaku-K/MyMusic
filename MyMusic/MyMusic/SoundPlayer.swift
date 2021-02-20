ca85c1f6e1cc66985112ea9aa394ef0d03473f58
Shusaku-K <77326356+Shusaku-K@users.noreply.github.com>
2021-02-21 04:05:05 +0900

    Update SoundPlayer.swift

--- MyMusic/MyMusic/SoundPlayer.swift
+++ MyMusic/MyMusic/SoundPlayer.swift
@@ -6,26 +6,31 @@
 //
 
 import UIKit
+//AVFoundation = 音を扱いやすくしてくれるフレームワーク。
 import AVFoundation
 
 class SoundPlayer: NSObject {
     
-    
+    // シンバルの音源データを読み込み
     let cymbalDate = NSDataAsset(name: "cymbalSound")!.data
-    
+    // シンバル用プレイヤーの変数
     var cymbalPlayer: AVAudioPlayer!
-    
+    // ギターの音源データを読み込み
     let guitarData = NSDataAsset(name: "guitarSound")!.data
-    
+    // ギター用プレイヤーの変数
     var guitarPlayer: AVAudioPlayer!
     
+    
+    // シンバルの音を鳴らすメソッド
     func cymbalPlay() {
         do {
+        // シンバル用のプレイヤーに、音源データを指定
         cymbalPlayer = try AVAudioPlayer(data: cymbalDate)
-        
+        //シンバルの音源再生
         cymbalPlayer.play()
             
         } catch {
+            //エラー処理
             print("シンバルで、エラーが発生しました！")
         }
         
@@ -33,10 +38,13 @@ class SoundPlayer: NSObject {
     
     func guitarPlay() {
         do {
+            // ギター用のプレイヤーに、音源ファイル名を指定
             self.guitarPlayer = try AVAudioPlayer(data: self.guitarData)
+            //ギターの音源再生
             self.guitarPlayer.play()
             
         } catch {
+            //エラー処理
             print("ギターで、エラーが発生しました！")
         }
     }
